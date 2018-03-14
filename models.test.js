f118daf254b64fc32b6c3dec3b60b7230b1c9010
unknown <brcampbell001@outlook.com>
2018-03-14 16:50:18 -0500

    Mini Horse Saddled

---
+++ models.test.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const chai = require('chai');
+const { expect } = chai;
+const sinon = require('sinon');
+
+const Band = require('./models')
+
+describe('Bands', () => {
+    describe('getBandName', () => {
+        it('should return the expected band name', () => {
+            const band = new Band({
+                name: 'Linkin Park',
+                genre: 'Whatever'
+            });
+            expect(band.getBandName()).to.equal('Linkin Park');
+        })
+    });
+    describe('getAllBands', () => {
+        it('should return all the bands', () => {
+            sinon.stub(Band, 'find');
+            Band.find.yields(null, [
+                { name: 'Linkin Park', genre: 'Whatever' },
+                { name: 'My Chemical Romance', genre: 'Emo-Rock' }
+            ]);
+            Band.getAllBands((bands) => {
+                expect(bands.length).to.equal(2);
+                expect(bands[1].name).to.equal('My Chemical Romance');
+                Band.find.restore();
+            });
+        });
+    });
+});
